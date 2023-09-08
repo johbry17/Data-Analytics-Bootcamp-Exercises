afb9a410b06b7083d7922cbb6e960e1fda59059a
johbry17 <73319490+johbry17@users.noreply.github.com>
2023-09-07 22:35:30 -0400

    14.1 almost done

---
+++ 14 javascript/14.1.10-Stu_Creating-Functions/static/js/app.js
@@ -0,0 +1,48 @@
+// Array of movie ratings
+let movieScore = [4.4, 3.3, 5.9, 8.8, 1.2, 5.2, 7.4, 7.5, 7.2, 9.7, 4.2, 6.9];
+
+// Mean is the average of all the values.
+function mean(thing) {
+    sum = 0.0;
+    for (let i=0; i < thing.length; i++) {
+        sum += thing[i];
+    }
+    return sum / thing.length;
+}
+
+
+// Variance can be found by subtracting the mean from each number in the data set,
+// squaring the result, and then averaging the square differences.
+function variance(thing) {
+    avg = mean(thing);
+    squares = [];
+    for (let i=0; i < thing.length; i++) {
+        squares.push((thing[i] - avg) * (thing[i] - avg));
+    }
+    return mean(squares)
+}
+
+
+
+// Standard deviation is the square root of the variance
+function std_dev(thing) {
+    return Math.sqrt(variance(thing));
+}
+
+
+// Print to the console the Movie Statistical Analysis
+function output(thing) {
+    console.log("The mean is " + mean(thing));
+    console.log('The variance is ' + variance(thing));
+    console.log('The standard deviation is ' + std_dev(thing));
+}
+
+output(movieScore);
+// Print to the console the Rainfall Statistical Analysis
+let monthlyAvgRainFall = [3.03, 2.48, 3.23, 3.15, 4.13, 3.23]
+output(monthlyAvgRainFall);
+
+
+// Print to the console the Running Statistical Analysis
+let mileRunTimes = [5.06, 4.54, 5.56, 4.40, 7.10]
+output(mileRunTimes);
--- 14 javascript/14.1.8-Stu_Multiple-Trace-Charts/plots.js
+++ 14 javascript/14.1.8-Stu_Multiple-Trace-Charts/plots.js
@@ -11,24 +11,46 @@ let romanSearchResults = []
 // For loop to populate arrays
 for (let i=0; i < searchResults.length; i++) {
     let x = searchResults[i];
-    
+
     names.push(x.pair)
     greekNames.push(x.greekName)
     romanNames.push(x.romanName)
     greekSearchResults.push(x.greekSearchResults)
     romanSearchResults.push(x.romanSearchResults)
-}
+};
 
 // Trace1 for the Greek Data
+let trace1 = {
+    x: names,
+    y: greekSearchResults,
+    name: 'Greek Gods',
+    type: 'bar',
 
+};
 
 // Trace 2 for the Roman Data
-
+let trace2 = {
+    x: names,
+    y: romanSearchResults,
+    name: 'Roman Gods',
+    type: 'bar',
+};
 
 // Create data array
-
+let data = [trace1, trace2];
 
 // Apply a title to the layout
-
+let layout = {
+    title: "Search Results, Greek vs. Roman Gods",
+    bargap: 0.2,
+    xaxis: {
+        title: "God Names",
+        tickfont: {
+            size: 14, // Adjust the font size
+        },
+        tickangle: -45, // Adjust the angle of the tick labels (e.g., -45 degrees for a slant)
+    },
+  };
 
 // Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
